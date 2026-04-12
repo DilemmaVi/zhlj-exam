415b9dd7fa585f30d41ba77cd1c7eb7601c2ed81
DilemmaVi <349371218@qq.com>
2026-04-13 07:13:35 +0800

    fix: restore missing if guard for localhost debug exports

--- app.js
+++ app.js
@@ -1180,7 +1180,7 @@ els.rpClearKey.addEventListener('click', () => {
   showKeyModal();
 });
 
-
+if (location.hostname === 'localhost' || location.hostname === '127.0.0.1') {
   window.startPractice = startPractice;
   window.startExam = startExam;
   window.startWrongBook = startWrongBook;
